9122d35bea7f8cb079664c81e119144f4fc710a0
Tiago Duarte da Cunha <tiago.cunha@aluno.fmpsc.edu.br>
2026-09-25 17:40:27 -0300

    Change showreel video URL to new YouTube link
    Updated showreel video URL to a new YouTube link.

--- data/media.ts
+++ data/media.ts
@@ -2,7 +2,7 @@
 // The showreel is now loaded from YouTube instead of a local MP4.
 export const media = {
   showreel: {
-    video: 'https://www.youtube.com/watch?v=Mj67WzDsaP4&t=1s',
+    video: 'https://www.youtube.com/watch?v=A1XsNJy-utU',
     thumbnail: '/images/project-motion.svg',
     platform: 'youtube'
   }
